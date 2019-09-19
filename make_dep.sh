6c095de63b29955ec47246bf893c266b3f33921b
sander <s.teunissen@gmail.com>
2019-09-19 16:48:12 +0200

    updating script

--- make_dep.sh
+++ make_dep.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+cd /app/fab-deb
+
+git pull
+
+python3.7 -m pip install /app/fab-dep/requirements.txt
+python3.7 -m pip install /app/fab-dep/requirements-dev.txt
+
 echo Running pyinstaller
 
 cd /app/fab-dep
 
-python3.7 -m pyinstaller fab.spec
+pyinstaller fab.spec
 
-cd dist/dist
+rm /app/fab/usr/bin/* -r
 
-cp * /app/fab_1.0-1/usr/bin/ -r
+cp /app/fab-dep/dist/fab/* /app/fab/usr/bin/ -r
+cp /app/fab-dep/control /app/fab/DEBIAN/
+
+cd /app
+dpkg-deb --build fab
